13d35416c9f84dbd6c2028b4e7eba121f0f48b6f
Kevin Hong <103216854+khongSC@users.noreply.github.com>
2022-05-27 13:57:24 -0700

    fix new_mini script

--- new_mini.sh
+++ new_mini.sh
@@ -15,5 +15,5 @@ done
 mkdir minis/"$name"
 cp -r minis/sample/{public,src,package.json,tsconfig.json} minis/"$name"
 echo "REACT_APP_APP_ID=${appId}" > minis/"$name"/.env.development
-sed "s/example/${name}/g; s/ee556332-8b38-4f7c-8b2c-04d77c23cc10/${appId}_default_md/g;" minis/sample/package.json > minis/"$name"/package.json
+sed "s/sample-mini/${name}/g; s/ee556332-8b38-4f7c-8b2c-04d77c23cc10/${appId}_default_md/g;" minis/sample/package.json > minis/"$name"/package.json
 sed "s/Example/${name}/g" minis/sample/public/index.html > minis/"$name"/public/index.html
